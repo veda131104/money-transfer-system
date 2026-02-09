c05031f24a034532a3948a94d67e10f671d306ef
Veda <veda.chatiyode@gmail.com>
2026-02-09 17:46:51 +0530

    Material UI

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
